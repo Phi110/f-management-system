bad8e50bcf5c3b98df9e6e6c15b7da9c551e8f0e
Phi110 <fuki150212@gmail.com>
2025-02-02 02:40:15 +0900

    Add files via upload

--- fms.js
+++ fms.js
@@ -1,19 +1,57 @@
-/* current time */
+const currentTime = new Date();
+const year = currentTime.getFullYear();
+const month = currentTime.getMonth();
+const date = currentTime.getDate();
+const day = currentTime.getDay();
+const hour = currentTime.getHours();
+const minute = currentTime.getMinutes();
+let period = "--";
 
-/* attendance */
+
+/* 現在時 */
+if ((day==6 || day==0)) {
+} else if ((hour==9 && minute>5) || (hour==10 && minute<=45)) {
+    period = 1;
+} else if ((hour==10 && minute>45) || (hour==11) || (hour==12 && minute<=25)) {
+    period = 2;
+} else if ((hour==13 && minute>10) || (hour==14 && minute<=50)) {
+    period = 3;
+} else if ((hour==14 && minute>50) || (hour==15) || (hour==16 && minute<=30)) {
+    period = 4;
+} else if ((hour==16 && minute>30) || (hour==18 && minute<=10)) {
+    period = 5;
+} else if ((hour==18 && minute>10) || (hour==19 && minute<=50)) {
+    period = 6;
+} else if ((hour==19 && minute>50) || (hour==20) || (hour==21 && minute<=30)) {
+    period = 7;
+}
+
+let li = [month+1, date, hour, minute];
+for (let i = 0; i < li.length; i++) {
+    let element = String(li[i])
+    if (element.length < 2) {
+        li[i] = "0" + element;
+    }
+}
+
+let wday = ["日", "月", "火", "水", "木", "金", "土"];
+const whattime = document.getElementById('whattime');
+whattime.innerHTML = `<h2><span class="emphasize">${li[0]}</span> /
+    <span class="emphasize">${li[1]}</span> 
+    <span class="right-space">(${wday[day]})</span> 
+    <span class="emphasize">${li[2]}</span> : 
+    <span class="emphasize">${li[3]}</span> ${period}限
+</h2>`
+
+
+/* 出席 */
 const urlContainer0 = document.getElementById('url-container0');
 const urlContainer1 = document.getElementById('url-container1');
 const urlContainer2 = document.getElementById('url-container2');
 const urlContainer3 = document.getElementById('url-container3');
-const currentTime = new Date();
-const day = currentTime.getDay();
-const hour = currentTime.getHours();
-const minute = currentTime.getMinutes();
-    
-/* 水曜日 */
+
 if (day == 3) {
-    /* 3限 13:10 - 14:50 */
-    if (hour == 13 && 10 <= minute && minute <= 50) {
+    if (period==3) {
         urlContainer0.src = "images/attendance1.webp";
         urlContainer1.innerHTML = '<h5 class="card-title"><a href="https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72622" target="_blank">コミュニケーションツールD（2024_火曜2限_IT1D）</a></h5>';
         urlContainer2.innerHTML = '<p class="card-text">後期(24_学部共)</p>';
@@ -21,10 +59,8 @@ if (day == 3) {
     }
 }
 
-/* 金曜日 */
 if (day == 5) {
-    /* 1限 9:05 - 4限 16:30 */
-    if ((hour == 9 && minute >= 5) || (10 <= hour && hour <= 15) || (hour == 16 && minute <= 30)) {
+    if (1<=period && period<=4) {
         urlContainer0.src = "images/attendance2.webp";
         urlContainer1.innerHTML = '<h5 class="card-title"><a href="https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=73031" target="_blank">組込みC, C++言語B（2024_月曜1・2限_IT1B/IT1D）</a></h5>';
         urlContainer2.innerHTML = '<p class="card-text">後期(24_共_IT)</p>';
@@ -32,7 +68,7 @@ if (day == 5) {
     }
 }
 
-/* checkanswers */
+/* 自動採点 */
 function checkAnswers(th, start, end) {
     let correctCount = 0;
     let totalCount = 0;
@@ -48,11 +84,11 @@ function checkAnswers(th, start, end) {
     }
 
     let markis, word;
-    let answerRate = correctCount / totalCount;
-    if (answerRate == 1) {
+    let correctRate = Math.floor(100*(correctCount / totalCount));
+    if (correctRate == 100) {
         markis = '<i class="bi bi-circle text-success right-space"></i>';
         word = '<span class="text-success">Perfect</span>';
-    } else if (answerRate >= 0.5) {
+    } else if (correctRate >= 50) {
         markis = '<i class="bi bi-triangle text-primary right-space"></i>';
         word = '<span class="text-primary">Good</span>';
     } else {
@@ -61,5 +97,5 @@ function checkAnswers(th, start, end) {
     }
 
     let score = document.getElementById("score" + String(th));
-    score.innerHTML = `<p>${markis}<strong>${correctCount}問</strong> / ${totalCount}問 正解です ${word}</p>`;
+    score.innerHTML = `<p>${markis}<strong>${correctCount}問</strong> / ${totalCount}問 (${correctRate}%) 正解です ${word}</p>`;
 }
