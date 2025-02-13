009b642644c57a047b8556228c6f540cb2c72d57
Phi110 <fuki150212@gmail.com>
2025-02-13 21:17:38 +0900

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 /* form.js */
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
