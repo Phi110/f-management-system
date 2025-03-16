b285395778a5e1451b933857d811b85588e1cda4
Phi110 <fuki150212@gmail.com>
2025-03-17 02:52:24 +0900

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,42 +1,38 @@
 /* form.js */
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCo3wV-0UysTzhSJuahnXxI29I4XryhYUI",
-  authDomain: "f-management-system.firebaseapp.com",
-  projectId: "f-management-system",
-  storageBucket: "f-management-system.firebasestorage.app",
-  messagingSenderId: "674332684290",
-  appId: "1:674332684290:web:19665cd604658ed565b71b",
-  measurementId: "G-90MFZR06SG"
+    apiKey: "AIzaSyCo3wV-0UysTzhSJuahnXxI29I4XryhYUI",
+    authDomain: "f-management-system.firebaseapp.com",
+    projectId: "f-management-system",
+    storageBucket: "f-management-system.firebasestorage.app",
+    messagingSenderId: "674332684290",
+    appId: "1:674332684290:web:19665cd604658ed565b71b",
+    measurementId: "G-90MFZR06SG"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function save_form() {
-    const comment = document.getElementById('comment').value;
-
-    if (!comment) {
-        alert("コメントを入力してください");
-        return;
-    }
-
+document.getElementById('surveyForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+  
+    const number = document.getElementById('number').value;
+    const name = document.getElementById('name').value;
+    const feedback = document.getElementById('feedback').value;
+  
     try {
-        await addDoc(collection(db, "comments"), {
-            text: comment,
+        await addDoc(collection(db, "surveys"), {
+            feedback: feedback,
+            number: number,
+            name: name,
             timestamp: new Date()
         });
-        alert("データが保存されました！");
-    } catch (error) {
-        console.error("エラー:", error);
+        alert('意見・要望が送信されました！');
+        document.getElementById('surveyForm').reset();
+    } catch (e) {
+        console.error("エラー: ", e);
     }
-}
+});
