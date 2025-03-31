d028503797fa53a915001b9a664296b26b9af717
Phi110 <fuki150212@gmail.com>
2025-04-01 01:30:28 +0900

    Add files via upload

--- js/fms.js
+++ js/fms.js
@@ -61,7 +61,6 @@ function getWeekNumber() {
     return weekNumber;
 }
 
-
 let studyroom = document.getElementById(`studyroom`);
 studyroom.src = "images/studyroom/studyroom" + (month+1) + "." + getWeekNumber() + ".webp";
 
@@ -89,10 +88,6 @@ class Attendance {
     get to_card() {
         let templates = [];
         for (let i = 0; i < this.course.length; i++) {
-            let dot = "・";
-            if (this.course[i] == "") {
-                dot = "";
-            }
             let picLink = `<img src="images/attendance/${this.picture[i]}.webp" class="card-img-top">`;
             let strLink = `${this.subject[i]}`;
             if (this.url[i] != "") {
@@ -107,7 +102,7 @@ class Attendance {
             }
             templates.push(
                 `<h3>
-                    ${dot}${this.course[i]}
+                    ・${this.course[i]}
                 </h3>
                 <div class="card">
                     ${picLink}
@@ -134,108 +129,128 @@ const a = new Attendance();
 if (day == 1) {
     if (period == 1) {
         // 共通
-        a.add("共通", "dj", "英語コミュニケーション Ⅱ aB", "345", "門田 裕次", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aB", "345", "門田 裕次", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aD", "342", "菅谷 孝義", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=78804");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aG", "376", "吉野 瑞男", "");
     }
     if (period == 2) {
         // 共通
-        a.add("共通", "dj", "英語コミュニケーション Ⅱ aA", "341", "津森 紀乃", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aA", "341", "津森 紀乃", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aC", "345", "門田 裕次", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aE", "342", "菅谷 孝義", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aF", "376", "吉野 瑞男", "");
     }
     if (period == 3) {
         // AI
-        a.add("AI", "programming", "プログラミング概論", "351", "齊藤亜・山口", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "programming", "プログラミング概論A", "351", "齊藤亜・山口", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=78892");
         // IoT
-        a.add("IoT", "rainy", "確率統計論", "342", "兒玉・野村", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "rainy", "確率統計論B", "342", "兒玉・野村", "");
     }
     if (period == 4) {
         // AI
-        a.add("AI", "rainy", "確率統計論", "351", "兒玉・野村", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "rainy", "確率統計論A", "351", "兒玉・野村", "");
     }
     if (period == 5) {
         // AI
-        a.add("AI", "tree", "自然言語処理", "351", "町出・野村", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "tree", "自然言語処理A", "351", "町出・野村", "");
     }
 }
 
 if (day == 2) {
     if (period == 1) {
         // IoT
-        a.add("IoT", "programming", "プログラミング概論", "351", "齊藤亜・山口", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "programming", "プログラミング概論B", "351", "齊藤亜・山口", "");
     }
     if (period == 2) {
         // IoT
-        a.add("IoT", "meeting", "IoTシステム開発 Ⅰ", "354", "藤井・山本裕", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "meeting", "IoTシステム開発 Ⅰ A", "354", "藤井・山本裕", "");
     }
     if (period == 3) {
         // IoT
-        a.add("IoT", "meeting", "IoTシステム開発 Ⅰ", "354", "藤井・山本裕", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "meeting", "IoTシステム開発 Ⅰ A", "354", "藤井・山本裕", "");
     }
     if (period == 4) {
         // AI
-        a.add("AI", "read", "人工知能システム開発 Ⅰ", "371・373", "神沼・齊藤亜", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "read", "人工知能システム開発 Ⅰ A", "371・373", "神沼・齊藤亜", "");
         // IoT
-        a.add("IoT", "meeting", "IoTシステム開発 Ⅰ", "354", "藤井・山本裕", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "meeting", "IoTシステム開発 Ⅰ A", "354", "藤井・山本裕", "");
     }
     if (period == 5) {
         // AI
-        a.add("AI", "read", "人工知能システム開発 Ⅰ", "371・373", "神沼・齊藤亜", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "read", "人工知能システム開発 Ⅰ A", "371・373", "神沼・齊藤亜", "");
         // IoT
-        a.add("IoT", "video", "担任ミーティング Ⅱ", "354", "兒玉 賢史", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "video", "担任ミーティング Ⅱ", "354", "兒玉 賢史", "");
         
     }
     if (period == 6) {
         // AI
-        a.add("AI", "video", "担任ミーティング Ⅱ", "371・373", "駒井 章治", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "video", "担任ミーティング Ⅱ", "371・373", "駒井 章治", "");
     }
 }
 
 if (day == 3) {
-    if (period == 1) {
+    if (period == 2 || period == 3) {
         // 共通
-        a.add("共通", "suit", "キャリアガイダンス Ⅰ", "-", "-", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "suit", "キャリアガイダンス Ⅰ", "-", "-", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=78899");
     }
-    if (2 <= period && period <= 5) {
+    if (4 <= period && period <= 5) {
         // 共通
-        a.add("共通", "club", "地域共創デザイン実習", "-", "-", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "club", "地域共創デザイン実習", "-", "-", "");
     }
 }
 
 if (day == 4) {
     if (period == 1) {
         // AI
-        a.add("AI", "exam", "人工知能数学", "311", "三宅・神田", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "exam", "人工知能数学A", "311", "三宅・神田", "");
         // IoT-A
-        a.add("IoT-A", "crane", "センサ・アクチュエータ", "361", "西田 麻美", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT-A", "crane", "センサ・アクチュエータA", "361", "西田 麻美", "");
     }
     if (period == 2) {
         // AI
-        a.add("AI", "speak", "人工知能基礎", "311", "齊藤亜・神田", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "speak", "人工知能基礎A", "311", "齊藤亜・神田", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=78895");
         // IoT-B
-        a.add("IoT-B", "crane", "センサ・アクチュエータ", "251", "西田 麻美", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT-B", "crane", "センサ・アクチュエータB", "251", "西田 麻美", "");
     }
     if (3 <= period && period <= 4) {
         // 共通
-        a.add("共通", "club", "地域共創デザイン実習", "-", "-", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "club", "地域共創デザイン実習", "-", "-", "");
     }
 }
 
 if (day == 5) {
     if (period == 1) {
         // 共通
-        a.add("共通", "dj", "英語コミュニケーション Ⅱ aB", "345", "門田 裕次", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aB", "345", "門田 裕次", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aD", "342", "菅谷 孝義", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=78804");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aG", "376", "吉野 瑞男", "");
     }
     if (period == 2) {
         // 共通
-        a.add("共通", "dj", "英語コミュニケーション Ⅱ aA", "341", "津森 紀乃", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aA", "341", "津森 紀乃", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aB", "345", "門田 裕次", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aE", "342", "菅谷 孝義", "");
+        // 共通
+        a.add("共通", "dj", "英語コミュニケーション Ⅱ aF", "376", "吉野 瑞男", "");
     }
     if (period == 3) {
         // AI
-        a.add("AI", "bookshelf", "データベース基礎と応用", "311", "飛澤・山口", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("AI", "bookshelf", "データベース基礎と応用A", "311", "飛澤・山口", "");
         // IoT
-        a.add("IoT", "power", "制御工学基礎", "351", "水上憲・中村", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "power", "制御工学基礎A", "351", "水上憲・中村", "");
     }
     if (period == 4) {
         // IoT
-        a.add("IoT", "bookshelf", "データベース基礎と応用", "351", "飛澤・山口", "https://lms-tokyo.iput.ac.jp/mod/attendance/view.php?id=72858");
+        a.add("IoT", "bookshelf", "データベース基礎と応用B", "351", "飛澤・山口", "");
     }
 }
 
